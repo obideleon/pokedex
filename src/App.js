23a93c646779765b760c8da709d4d0837ef24344
obideleon <robbydeleon@gmail.com>
2022-06-17 08:29:09 +0800

    Added MUI, Router, and Initial Details and Results Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
+import {
+  AppBar,
+  CssBaseline,
+  Link as MaterialLink,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Link as RouterLink,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Details from "./components/Details";
+import Reults from "./components/Results";
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <CssBaseline />
+      <BrowserRouter>
+        <AppBar
+          position="absolute"
+          color="default"
+          elevation={0}
+          sx={{
+            position: "relative",
+            borderBottom: (t) => `1px solid ${t.palette.divider}`,
+          }}
+        >
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>
+              <MaterialLink component={RouterLink} to="/">
+                Pokedex
+              </MaterialLink>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Routes>
+          <Route path="/details" element={<Details />} />
+          <Route path="/" element={<Reults />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 render(<App />, document.getElementById("root"));
---
+++ src/components/Results.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Reults = () => {
+  return (
+    <div>
+      <div>List of Pokemon</div>
+      <Link to="/details">Go to Details</Link>
+    </div>
+  );
+};
+
+export default Reults;
